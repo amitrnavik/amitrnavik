c683a0d466ba24c90dc379d1f052430530563f17
Amit Navik <amit.navik195@gmail.com>
2021-07-19 23:22:48 +0530

    removed encryption added hashing

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const express= require("express");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
@@ -15,8 +15,8 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+// const secret = ;
+// userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -39,7 +39,7 @@ app.get("/login",function (req,res) {
  app.post("/register",function(req,res){
      const newUser = new User({
          email : req.body.username,
-        password :req.body.password
+        password :md5(req.body.password)
      });
      newUser.save(function (err) {
         if(err){
@@ -53,7 +53,7 @@ app.get("/login",function (req,res) {
 
  app.post("/login",function(req,res){
      const username = req.body.username;
-     const password= req.body.password;
+     const password= md5(req.body.password);
 
      User.findOne({email:username},function(err,foundUser){
         if(err){
